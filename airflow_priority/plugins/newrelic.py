de65f7d9724d18cddcca4713f921addfac9ff3de
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-05-30 17:24:51 -0400

    Support custom tags for newrelic and datadog, fixes #52

--- airflow_priority/plugins/newrelic.py
+++ airflow_priority/plugins/newrelic.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+from json import loads
 from typing import Any, Dict
 
 from newrelic_telemetry_sdk import GaugeMetric, MetricClient
@@ -17,56 +18,49 @@ def get_client():
 
 def send_metric(dag_id: str, priority: int, tag: DagStatus, context: Dict[DagStatus, Any]) -> None:
     metric = get_config_option("newrelic", "metric", default=DefaultMetric)
-    metrics = [GaugeMetric(f"{metric}.p{priority}.{tag}", 1, tags={"application": "airflow", "dag": dag_id, "priority": priority, "tag": tag})]
+    tags = loads(get_config_option("newrelic", "tags", default="{}"))
+    tags = {
+        "application": "airflow",
+        "priority": str(priority),
+        "dag": dag_id,
+        **tags,
+    }
+    metrics = [GaugeMetric(f"{metric}.p{priority}.{tag}", 1, tags=tags)]
 
     if tag == "success":
         if "running" in context:
             # If the task was running before, we need to decrement the running metric
             metrics.append(
                 GaugeMetric(
-                    f"{metric}.p{priority}.running", 0, tags={"application": "airflow", "dag": dag_id, "priority": priority, "tag": "running"}
+                    f"{metric}.p{priority}.running",
+                    0,
+                    tags=tags,
                 )
             )
             context.pop("running", None)
         if "failed" in context:
             # If the task was failed before, we need to decrement the failed metric
-            metrics.append(
-                GaugeMetric(f"{metric}.p{priority}.failed", 0, tags={"application": "airflow", "dag": dag_id, "priority": priority, "tag": "failed"})
-            )
+            metrics.append(GaugeMetric(f"{metric}.p{priority}.failed", 0, tags=tags))
             context.pop("failed", None)
         context.clear()
     elif tag == "failed":
         # If the task was running before, we need to decrement the running metric
         if "running" in context:
-            metrics.append(
-                GaugeMetric(
-                    f"{metric}.p{priority}.running", 0, tags={"application": "airflow", "dag": dag_id, "priority": priority, "tag": "running"}
-                )
-            )
+            metrics.append(GaugeMetric(f"{metric}.p{priority}.running", 0, tags=tags))
             context.pop("running", None)
         if "success" in context:
             # If the task was successful before, we need to decrement the success metric
-            metrics.append(
-                GaugeMetric(
-                    f"{metric}.p{priority}.success", 0, tags={"application": "airflow", "dag": dag_id, "priority": priority, "tag": "success"}
-                )
-            )
+            metrics.append(GaugeMetric(f"{metric}.p{priority}.success", 0, tags=tags))
             context.pop("success", None)
         context["failed"] = True
     elif tag == "running":
         if "success" in context:
             # If the task was successful before, we need to decrement the success metric
-            metrics.append(
-                GaugeMetric(
-                    f"{metric}.p{priority}.success", 0, tags={"application": "airflow", "dag": dag_id, "priority": priority, "tag": "success"}
-                )
-            )
+            metrics.append(GaugeMetric(f"{metric}.p{priority}.success", 0, tags=tags))
             context.pop("success", None)
         if "failed" in context:
             # If the task was failed before, we need to decrement the failed metric
-            metrics.append(
-                GaugeMetric(f"{metric}.p{priority}.failed", 0, tags={"application": "airflow", "dag": dag_id, "priority": priority, "tag": "failed"})
-            )
+            metrics.append(GaugeMetric(f"{metric}.p{priority}.failed", 0, tags=tags))
             context.pop("failed", None)
         context["running"] = True
     get_client().send_batch(metrics)
