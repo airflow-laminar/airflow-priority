e63feb5f7ea029a1e26184d575253aba1baf2d70
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-06-10 19:33:50 -0400

    Support new airflow-config priority setting

--- airflow_priority/tests/test_airflow_config.py
+++ airflow_priority/tests/test_airflow_config.py
@@ -1,6 +1,20 @@
 from pathlib import Path
 
+import pytest
+
 conf_text = """# @package _global_
+_target_: airflow_config.Configuration
+default_args:
+  _target_: airflow_config.TaskArgs
+  owner: test
+priority:
+  _target_: airflow_priority.PriorityConfiguration
+  slack:
+    token: abc
+    channel: def
+"""
+
+conf_text2 = """# @package _global_
 _target_: airflow_config.Configuration
 default_args:
   _target_: airflow_config.TaskArgs
@@ -14,26 +28,31 @@ extensions:
 """
 
 
-def test_config_loading_via_airflow_config(airflow_config, dag_run):
+@pytest.mark.parametrize("contents", [conf_text, conf_text2])
+def test_config_loading_via_airflow_config(airflow_config, dag_run, contents):
     dags_path = Path(airflow_config) / "dags"
     conf_path = dags_path / "config"
     conf_path.mkdir(parents=True, exist_ok=True)
     conf_file = conf_path / "config.yaml"
-    conf_file.write_text(conf_text)
+    conf_file.write_text(contents)
 
     from airflow_config import Configuration
 
     config = Configuration.load("config", "config", basepath=str(dags_path), _offset=4)
-    ret = getattr(getattr(config.extensions.get("priority", None), "slack", None), "token", None)
+    if contents == conf_text:
+        ret = getattr(getattr(config.priority, "slack", None), "token", None)
+    else:
+        ret = getattr(getattr(config.extensions.get("priority", None), "slack", None), "token", None)
     assert ret == "abc"
 
 
-def test_call_plugin_via_airflow_config(airflow_config, dag_run):
+@pytest.mark.parametrize("contents", [conf_text, conf_text2])
+def test_call_plugin_via_airflow_config(airflow_config, dag_run, contents):
     dags_path = Path(airflow_config) / "dags"
     conf_path = dags_path / "config"
     conf_path.mkdir(parents=True, exist_ok=True)
     conf_file = conf_path / "config.yaml"
-    conf_file.write_text(conf_text)
+    conf_file.write_text(contents)
     from airflow_priority.plugins.slack import get_client
 
     client = get_client()
